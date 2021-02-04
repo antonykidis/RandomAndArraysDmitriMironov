11e8324323822139333fbc176de5cc7531681814
antony.kidis <antony.kidis@gmail.com>
2021-02-04 19:13:59 +0200

    Add project files.

---
+++ Answer02/Answer02.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+//Coded By Dmitry Mironov QA 1021 course  2/4/2021
+//Answer 02
+//------------------------------------------------
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Answer02
+{
+    class Answer02
+    {
+        static List<string[]> MyList = new List<string[]>();
+        static List<string> MySortedList = new List<string>();
+        static bool toSorAscending;
+
+        static void Main(string[] args)
+        {
+            MyList = CreateThreeArrays();         //create 3 arrays and "put" them into a List
+            SimplePrint(MyList);                  //Print 3 Unconcatinated  arrays on the screen.
+            toSorAscending = AskUserHowToSort();  //Ask how to sort
+
+            //Passing 3 arrays to a method + toSorAscending bool........ AS REQUESTED!
+            MySortedList = Sort(toSorAscending, MyList);
+            PrintMySortedList(MySortedList, toSorAscending); //Print based on selection
+        }
+
+        //Custom methods...........................................
+        private static void PrintMySortedList(List<string> _List, bool _isSroted)
+        {
+            if (_isSroted)
+            {
+                //print sorted
+                Console.WriteLine("\n\nHere is: Sorted And CONCATENATED ARRAY:\n");
+                foreach (var item in _List)
+                {
+                    Console.Write("\t" + item);
+                }
+                Console.WriteLine("\n\nPress any key to exit..");
+                Console.ReadLine();
+            }
+            else
+            {
+                //print sorted
+                Console.WriteLine("\n\nHere is: UNSORTED CONCATENATED ARRAY:\n");
+                foreach (var item in _List)
+                {
+                    Console.Write("\t" + item);
+                }
+                Console.WriteLine("\n\nPress any key to exit..");
+                Console.ReadLine();
+            }
+        } //done
+
+        private static List<string> Sort(bool toSorAscending, List<string[]> _myList)
+        {
+            List<string> concList = new List<string>();
+
+            //Concatinating the strings into one single List
+            foreach (var MyCollection in _myList)
+            {
+                foreach (var item in MyCollection)
+                {
+                    concList.Add(item);
+                }
+            }
+            if (toSorAscending)
+            {
+                //do the sort
+                concList.Sort();
+            }
+
+            return concList;
+        } //done
+
+        private static bool AskUserHowToSort()
+        {
+            bool IsAscending;
+            Console.WriteLine("\n\nPress [y] button to Sort, or press any other key to proceed without sorting");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                IsAscending = true;
+            }
+            else
+            {
+                IsAscending = false;
+            }
+            return IsAscending;
+        } //done
+
+        private static List<string[]> CreateThreeArrays()
+        {
+            List<string[]> MyArrayList = new List<string[]>();
+            string[] arr1 = new string[] { "F", "H", "E", "G", "D", "J", "C", "I", "B", "A", "dog" };
+            string[] arr2 = new string[] { "N", "M", "K", "", "L", "lazy", "the", "O", "X", "P", "quick" };
+            string[] arr3 = new string[] { "Y", "Q,", "over", "Z", "", "The", "fox", "", "jumps", "brown" };
+            MyArrayList.Add(arr1);
+            MyArrayList.Add(arr2);
+            MyArrayList.Add(arr3);
+            //The quick brown fox jumps over the lazy dog
+            return MyArrayList;
+        } //done
+
+        private static void SimplePrint(List<string[]> _arrayList)
+        {
+            Console.WriteLine("\n Here are 3  UNCONCATENATE ARRAYS:");
+            int ctr = 1;                                             
+            foreach (var ListOfArrays in _arrayList)
+            {
+                Console.Write("\n\nArray: {0}\n", ctr);
+                foreach (var item in ListOfArrays)
+                {
+                    Console.Write("\t" +"["+ item +"]");
+                }
+                ctr++;
+            }
+        } //done
+    }
+
+}
---
+++ RandomAndArraysDmitriMironov/Answer01.cs
@@ -0,0 +1,203 @@
+﻿//Coded By Dmitry Mironov QA 1021 course  2/4/2021
+//Answer 02
+//------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomAndArraysDmitriMironov
+{
+
+    class Answer01
+    {
+
+        static List<int> SortedArr = new List<int>();
+        static Dictionary<int, int> PairsCollection = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[100];
+
+
+            //print the array to user
+            PrintRandomNumbers(myArray);
+
+            //Sort the array before continue.
+            SortedArr = SortMyArray(myArray);
+
+            //Search for repeated items
+            //using Dictionary Generics object
+            PairsCollection = SearchRepeatedValue(SortedArr);
+
+            //Print the repeated values
+            PrintRepeatedValues(PairsCollection);
+
+            //Print Max
+            PrintMax(PairsCollection);
+
+            //Print Min
+            PrintMin(PairsCollection);
+
+            //Print Average
+            double Average = CalculateAverage(PairsCollection);
+            PrintAverage(Average);
+
+        }
+
+
+        //Custom methods............................................
+        static double CalculateAverage(Dictionary<int, int> _dictionary)
+        {
+            double SumOfDictionary = 0;
+            int NumberOfItemsInDictionary = _dictionary.Count;
+
+            foreach (var item in _dictionary)
+            {
+                SumOfDictionary += item.Key;                   //Key is Number
+            }                                                  //Value is number's appearances in total
+                                                               //Calculate average
+                                                               //Don't forget to cast to double!!!!
+            double sum = SumOfDictionary / (double)NumberOfItemsInDictionary;
+            return sum;
+        } //done
+
+        static void PrintRepeatedValues(Dictionary<int, int> _dictionary)
+        {
+            foreach (var Pair in _dictionary)
+            {
+                //Key= Number
+                //Value= Number of appearances of this number
+                Console.WriteLine("Value {0} occurred {1} times.", Pair.Key, Pair.Value);
+            }
+        } //done
+
+        static Dictionary<int, int> SearchRepeatedValue(List<int> _listArr)
+        {
+            Dictionary<int, int> MyDictionary = new Dictionary<int, int>();
+            foreach (var item in _listArr)
+            {
+                if (MyDictionary.ContainsKey(item)) //if my dictionary's (KEY/Value) =>Value's-value  contains a similar number, just increase the counter.
+                {
+                    MyDictionary[item]++;
+                }
+                else
+                {
+                    MyDictionary[item] = 1; //No, my dictionary doesn't contain this value add it to the Pair Key
+                }
+
+                //Doing this we end up with a fully completed Key-Value pairs Dictionary Object
+                //Containing numbers'-amount, and their repetition's amount :-)
+            }
+            //return  MyDictionary Object
+            return MyDictionary;
+        } //done
+
+        static void PrintMax(Dictionary<int, int> _pairsCollection)
+        {
+
+            int MaximumAppearences = 0;  //get the key based on the MaximumAppearences number
+            int OfThisNumer = 0;
+
+            //First get maximum appearances amount
+            foreach (var item in _pairsCollection.Values)
+            {
+                if (item > MaximumAppearences)
+                {
+                    MaximumAppearences = item;
+                }
+            }
+            //get the MaximumAppearance value by finding it within _pairsCollection Dictionary
+            foreach (var item in _pairsCollection)
+            {
+                if (item.Value == MaximumAppearences)
+                {
+                    //get its key
+                    //I Found it, lets add it to OfThisNumer variable
+                    OfThisNumer = item.Key;
+                }
+            }
+
+            //Find the greates number of all
+            int greatestNumber = 0;
+            foreach (var item in _pairsCollection.Keys)
+            {
+                if (item > greatestNumber)
+                {
+                    greatestNumber = item;
+                }
+            }
+            Console.WriteLine("\nThe Number {0} is appeared {1} times\n" +
+                "So the number with maximum appearances is: {2}", OfThisNumer, MaximumAppearences, OfThisNumer);
+            Console.WriteLine("\nthe Max num is {0}", greatestNumber);
+            //Console.Read();
+
+        }  //done
+
+        static void PrintMin(Dictionary<int, int> _pairsCollection)
+        {
+            //Don't forget to import use System.Linq to make OrderBy extension method to work :-)
+            //Finding the very first Item within Dictionary Object.
+            var SmallestKeyValuePair = _pairsCollection.OrderBy(kvp => kvp.Key).First();
+            int smallestNumber = SmallestKeyValuePair.Key;
+
+            foreach (var item in _pairsCollection.Keys)
+            {
+                if (item < smallestNumber)
+                {
+                    smallestNumber = item;
+                }
+            }
+            Console.WriteLine("\nThe smallest number is {0}\n", smallestNumber);
+            //Console.Read();
+
+        }  //done!
+
+        static void PrintAverage(double _param)
+        {
+            //Format decimal to represent only 3 digits after point
+            Console.WriteLine("\nThe Average is: {0,2:#.000}", _param);
+            Console.WriteLine("\npress any key to exit...");
+            Console.Read();
+        } //done
+
+        static List<int> SortMyArray(int[] _arr)
+        {
+            List<int> SortedList = new List<int>();
+
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                //Fill the SortedArrList
+                SortedList.Add(_arr[i]);
+            }
+            //Now simply Sort the List
+            SortedList.Sort(); //will sort in Ascending
+
+            return SortedList;
+        } //done
+
+        static void PrintRandomNumbers(int[] _aray)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < _aray.Length; i++)
+            {
+                _aray[i] = rnd.Next(1, 50); //You can change the values between 0,50 it also works!!! I wanted to omit the zeros on the print
+            }
+
+            //print the 100 numbers to screen.
+            Console.WriteLine("Hello User!\nYour array contains the following 100 Random numbers. Each fom 0 to 50:\n");
+            for (int i = 0; i < _aray.Length; i++)
+            {
+                Console.Write("{0}\t ", _aray[i]); //print each item
+            }
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadLine();
+
+
+        } //done
+
+
+    }
+}
